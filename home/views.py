894e5a330512578da471845d2938f6c277316eb5
abahjnr <abahernesto@gmail.com>
2020-05-04 01:00:32 +0100

    final changes to website features, added moreposts.html and morestories.html

--- home/views.py
+++ home/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Blog,RAGP_Carousel,RAGP_Stories
 
+
+blogs=Blog.objects.all().order_by('-date')
+carousels=RAGP_Carousel.objects.all()
+stories=RAGP_Stories.objects.all().order_by('-id')
+
+
+
 # Create your views here.
 def HomeViews (request):
-    blogs=Blog.objects.all()
-    carousels=RAGP_Carousel.objects.all()[1:]
-    stories=RAGP_Stories.objects.all()
-    firstcarousel=RAGP_Carousel.objects.all()[0]
-
-    context= {'blogs':blogs,'carousels':carousels,'firstcarousel':firstcarousel,'stories':stories}
+    context= {'blogs':blogs[:5],'carousels':carousels[1:],'firstcarousel':carousels[0],'stories':stories[:2]}
     return render (request,'home/index.html', context)
 
 def AboutViews (request):
     return render(request,'home/about.html')
 
 def DetailViews (request, blog_title):
-    blogs=get_object_or_404(Blog, title=blog_title)
+    blog_detail=get_object_or_404(Blog, title=blog_title)
 
-    context={'blog':blogs}
+    context={'blog':blog_detail}
     return render(request,'home/detail.html',context)
 
 def StoryViews (request,ragp_stories_name):
-    stories=get_object_or_404(RAGP_Stories, name=ragp_stories_name)
+    stories_detail=get_object_or_404(RAGP_Stories, name=ragp_stories_name)
 
-    context={'story':stories}
+    context={'story':stories_detail}
     return render(request,'home/stories.html',context)
+
+def MoreStoriesViews (request):
+    context={'stories':stories}
+    return render(request,'home/morestories.html',context)
+
+
+def MorePostsViews (request):
+    context={'blogs':blogs}
+    return render(request,'home/moreposts.html',context)
--- wealthline_merchants/urls.py
+++ wealthline_merchants/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import HomeViews,DetailViews,AboutViews,StoryViews
+from home.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -25,4 +25,6 @@ urlpatterns = [
     path('about-Us/',AboutViews,name="about"),
     path('blog/<blog_title>/',DetailViews,name="detail"),
     path('story/<ragp_stories_name>/', StoryViews, name='story'),
+    path('blog/',MoreStoriesViews,name="morestories"),
+    path('story/',MorePostsViews,name="moreposts"),
 ]
